da16ce7c4fcda7c0e85b1e41f36cccefd69519ed
Temi <limiajayi@gmail.com>
2026-05-01 21:25:24 +0100

    FAILED.

--- index.mjs
+++ index.mjs
@@ -1,8 +1,21 @@
 import ollama from 'ollama';
+import { z } from 'zod';
+import { zodToJsonSchema } from 'zod-to-json-schema';
+
+const Country = z.object({
+    name: z.string(),
+    capital: z.string(),
+    languages: z.array(z.string()),
+});
 
 const response = await ollama.chat({
     model: 'gemma3:270m',
-    messages: [{role: 'user', content: 'Hello!'}],
+    messages: [{
+        role: 'user', 
+        content: `Tell me about Nigeria`
+    }],
+    format: 'json',
 });
 
-console.log(response.message.content);
+const country = Country.parse(JSON.parse(response.message.content));
+console.log(country);
